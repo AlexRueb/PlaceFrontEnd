0fd547b6720186f80be6b41b326260378c38a046
dtice <dillon.tice@gmail.com>
2019-11-12 21:21:07 -0700

    .

--- index.php
+++ index.php
@@ -14,54 +14,50 @@ $canvas = $canvas_select_stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
 <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="UTF-8">
-    <title>Place</title>
-    <link href="style.css" rel="stylesheet">
-    <script src="index.js" defer></script>
-</head>
+  <head>
+      <meta charset="UTF-8">
+      <title>Place</title>
+      <link href="style.css" rel="stylesheet">
+      <script src="index.js" defer></script>
+  </head>
 
-<body>
-    <header>The Place</header>
-    <div id="container">
-        <nav>Nav
-        <ul>
-            <li>About Us</li>
-            <li>History</li>
-        </ul>
-        </nav>
+  <body>
+      <header>The Place</header>
+      <div id="container">
+          <nav>Nav
+          <ul>
+              <li>About Us</li>
+              <li>History</li>
+          </ul>
+          </nav>
 
-        <main>
-            <div id="info"></div>
-            <div class="pixelCanvas">
-            <!-- TODO: Create a FlexBox that wraps after every x pixels -->
-            <?php
-              foreach($canvas as $pixel){ ?>
-                <!-- Not sure if attributes are the best way to set x and y -->
-                <div class="pixel" db_id="<?=$pixel['id'];?>" color="<?=$pixel['color'];?>" style="background-color: <?=$pixel['color'];?>"></div>
-              <?php }
-            ?>
-            </div>
-        </main>
+          <main>
+              <div class="pixelCanvasContainer">
+                <div class="pixelCanvas">
+                <!-- TODO: Create a FlexBox that wraps after every x pixels -->
+                <?php
+                  foreach($canvas as $pixel){ ?>
+                    <!-- Not sure if attributes are the best way to set x and y -->
+                    <div class="pixel" db_id="<?=$pixel['id'];?>" color="<?=$pixel['color'];?>" style="background-color: <?=$pixel['color'];?>"></div>
+                  <?php }
+                ?>
+                </div>
+              </div>
+          </main>
 
-        <aside>
-			<form>
-			<label>Pick your color!</label>
-				<input id="colorChoice" type="color" value="#000000" />
-				<input type="submit" />
-            </form>
-			<p>Instructions:</p>
+          <aside>
+            <label>Pick your color!</label><br>
+            <input id="colorChoice" type="color" value="#000000" />
+            <p>Instructions:</p>
             <ol>
                 <li>Click the color box</li>
                 <li>Pick your desired color</li>
-                <li>Click submit</li>
                 <li>Click any pixel in the grid to instantly change it to your color!</li>
             </ol>
-        </aside>
-    </div>
+          </aside>
+        </div>
         <footer>Footer</footer>
-
-</body>
+  </body>
 </html>
 <?php
 ?>
